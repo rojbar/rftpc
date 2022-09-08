51a3e8a1e38e70f5fd41fd77aa2ec39e6632c7f3
rojbar <55609284+rojbar@users.noreply.github.com>
2022-09-08 11:23:11 -0500

    fixed path shown to user

--- cmd/root.go
+++ cmd/root.go
@@ -52,7 +52,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.rftp/config.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.rftpc/config.yaml)")
 
 }
 
